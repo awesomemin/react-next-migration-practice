92b2946dcec5cb4fc0b40cd2a73eeec5c477e6e0
Seongmin Oh <saogig0503@naver.com>
2026-02-01 21:17:30 +0900

    Router 에러, 환경변수 import 에러 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import { BrowserRouter } from 'react-router-dom';
 import '../index.css';
 import dynamic from 'next/dynamic';
 const App = dynamic(() => import('../App'), { ssr: false });
 
 export default function Page() {
-  return <App />;
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
